c5b2db2a9ac92d9de49eb24a0a69682b8c1684dc
Sesmi Athiyarath <sesmi.athiyarath@siemens-healthineers.com>
2024-03-27 02:18:06 +0530

    update sliding window counter

--- rate-limiter/rate_limiter/sliding_window_counter/sliding_window_counter.py
+++ rate-limiter/rate_limiter/sliding_window_counter/sliding_window_counter.py
@@ -7,9 +7,10 @@ class SlidingWindowCounter:
         self.request_limit = request_limit
         self.window_size_in_seconds = window_size_in_seconds
         
-        self.cur_time = int(time())
-        self.cur_count = 0
-        self.window = []
+        self.current_window_start = int(time())
+        self.current_window_request_count = 0
+        self.previous_window_start = int(time()) - window_size_in_seconds
+        self.previous_window_request_count = 0
         
     def _validate_parameters(self, request_limit: int, window_size_in_seconds: int) -> None:
         if request_limit <= 0 or \
@@ -19,21 +20,30 @@ class SlidingWindowCounter:
                 raise ValueError("request_limit and window_size_in_seconds should be natural numbers")
 
     def allow_request(self):
-        # Check if it's a new time unit
-        if (int(time()) - self.cur_time) > self.window_size_in_seconds:
-            self.cur_time = int(time())
-            self.window = [self.cur_count]
-            self.cur_count = 0
+        current_time = int(time())
+        if self.current_window_start + self.window_size_in_seconds > current_time:
+            # We are within the current window
+            prev_window_weight = ( self.current_window_start - (current_time - self.window_size_in_seconds) ) / self.window_size_in_seconds
+            effective_capacity = self.current_window_request_count + 1 + prev_window_weight * self.previous_window_request_count
+            if effective_capacity > self.request_limit:
+                return False
+            else:
+                self.current_window_request_count += 1
+                return True
         else:
-            self.window.append(self.cur_count)
-
-        effective_capacity = sum((self.request_limit - count) * \
-                                (self.window_size_in_seconds - idx * self.window_size_in_seconds / len(self.window)) / \
-                                self.window_size_in_seconds for idx, count in enumerate(self.window))
-        
-        # Check if the effective capacity is exceeded
-        if effective_capacity > self.request_limit:
-            return False
-
-        self.cur_count += 1
-        return True
+            # We are in a new window
+            if self.current_window_start + 2 * self.window_size_in_seconds > current_time:
+                # New window is adjacent to current window
+                self.previous_window_request_count = self.current_window_request_count
+                self.previous_window_start = self.current_window_start
+                self.current_window_request_count = 1
+                self.current_window_start = self.previous_window_start + self.window_size_in_seconds
+                return True
+            else:
+                # New window is not adjacent to the current window
+                n = int((current_time - self.current_window_start)/self.window_size_in_seconds)
+                self.current_window_start = self.current_window_start + self.window_size_in_seconds * n
+                self.current_window_request_count = 1
+                self.previous_window_start = self.current_window_start - self.window_size_in_seconds
+                self.previous_window_request_count = 0
+                return True
